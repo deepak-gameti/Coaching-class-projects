c14b11a5ad108224de6e0ac2e02eb08d00d802dc
deepak-gameti <159270881+deepak-gameti@users.noreply.github.com>
2024-03-16 20:01:22 +0530

    js programm

---
+++ JavaScriptProgram-1/index.js
@@ -0,0 +1,94 @@
+
+
+
+
+
+
+// -----------------------------------------
+
+// Programm for find which number is big or greater then other
+
+// let Num1 = prompt(`Enter first number here`);
+// let Num2 = prompt(`Enter second number here`);
+
+// if(Num1 > Num2){
+//     alert(`${Num1} is greater then ${Num2}`)
+// }
+// else if(Num1 < Num2){
+//     alert(`${Num1} is smaller then ${Num2}`)
+// }
+// else if(Num1 === Num2){
+//     alert(`${Num1} equal to ${Num2}`)
+// }
+// else{
+//     alert(`Enter valid number please`)
+// }
+
+
+
+
+
+
+// -----------------------------------------------
+
+// Program for find leap year
+
+// let year = prompt(`Enter year here for check leap year`);
+
+// if(year % 4 === 0){
+//     alert(`${year} year is leap year`)
+// }
+// else{
+//     alert(`${year} is not leap year`)
+// }
+
+
+
+// -----------------------------------------
+
+// program for find ticket prise according to age
+// let age = prompt(`enter your age here for know your ticket prise`);
+
+// const ticketPrise1 = "$5";
+// const ticketPrise2 = "$10";
+// const ticketPrise3 = "$20";
+// const ticketPrise4 = "$15";
+
+// if (age <= 12 && age > 5) {
+//     console.log(`your ticket prise is ${ticketPrise1}`)
+//     // console.log(`your ticket prise is 5rs. ${ticketPrise1}`)
+// }
+// else if (age <= 18 && age > 12) {
+//     console.log(`your ticket prise is ${ticketPrise2}`)
+// }
+// else if (age <= 60 && age > 18) {
+//     console.log(`your ticket prise is ${ticketPrise3}`)
+// }
+// else if (age > 60 && age <= 100) {
+//     console.log(`your ticket prise is ${ticketPrise4}`)
+// }
+// else{
+//     alert(`invalid age`)
+// }
+
+
+// -----------------------------------
+
+
+
+// Program for check given number is positive or nagative
+// let number = prompt('Enter any number here');
+
+
+// if (number >= 0) {
+//     alert("this number is positive");
+// }
+// else if (number < 0) {
+//     alert("this is nagative number");
+// }
+// else {
+//     alert("Enter number please");
+// }
+
+// console.log(number);
+
